e4b5fb261b6fce1d913f94ee061e9976911f1612
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-03 18:12:08 -0300

    feat: Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import IsleCard from '../templates/IsleCard';
+import SearchForm from '../templates';
+
+export default function Home() {
+  return (
+    <>
+      <SearchForm />
+      <IsleCard />
+    </>
+  );
+}

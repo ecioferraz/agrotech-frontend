ee74db8a7077ea19cbe69fcd9c572fdbbf9b788a
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-03 16:55:18 -0300

    feat: StatusCard component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Button from './Button';
+import StatusCard from './StatusCard';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
 
 export {
   Button,
+  StatusCard,
   TextCard,
   TextInput,
 };

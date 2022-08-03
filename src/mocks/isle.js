ecbeae0a7c79242324b15d230790297c31a5a33c
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-03 18:55:36 -0300

    feat: isle mock

---
+++ src/mocks/isle.js
@@ -0,0 +1,40 @@
+const isleMock = [
+  {
+    name: 'Azul',
+    status: 'Ativa',
+    temperature: 20.5,
+    airHumidite: 80,
+    soilHumidite: 30,
+    dateTime: '03/08/2022',
+    imageURL: '',
+  },
+  {
+    name: 'Vermelha',
+    status: 'Ativa',
+    temperature: 25.4,
+    airHumidite: 30.9,
+    soilHumidite: 26,
+    dateTime: '03/08/2022',
+    imageURL: '',
+  },
+  {
+    name: 'Verde',
+    status: 'Inativa',
+    temperature: 31,
+    airHumidite: 80,
+    soilHumidite: 60,
+    dateTime: '03/08/2022',
+    imageURL: '',
+  },
+  {
+    name: 'Laranja',
+    status: 'Ativa',
+    temperature: 12.6,
+    airHumidite: 34.1,
+    soilHumidite: 68.9,
+    dateTime: '03/08/2022',
+    imageURL: '',
+  },
+];
+
+export default isleMock;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import IsleCard from '../templates/IsleCard';
-import SearchForm from '../templates';
+import { SearchForm } from '../templates';
+import isleMock from '../mocks/isle';
 
 export default function Home() {
   return (
     <>
       <SearchForm />
-      <IsleCard />
+      { isleMock.map((isle) => (
+        <IsleCard
+          key={isle.name}
+          imageURL={isle.imageURL}
+          status={isle.status}
+          temperature={isle.temperature}
+          airHumidite={isle.airHumidite}
+          soilHumidite={isle.soilHumidite}
+          dateTime={isle.dateTime}
+        />
+      )) }
     </>
   );
 }

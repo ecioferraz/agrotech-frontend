c2328cab139048cb9b509ed4c97ae0f0027bae3a
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-04 21:45:45 -0300

    refactor: APIRequests

--- src/services/APIRequests.js
+++ src/services/APIRequests.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const { REACT_APP_HOSTNAME: HOST, REACT_APP_BACKEND_PORT: PORT } = process.env;
+// const { REACT_APP_HOSTNAME: HOST } = process.env;
 
-const baseURL = `http://${HOST || 'localhost'}:${PORT || '3001'}`;
-
-const api = axios.create({ baseURL });
+const api = axios.create({
+  baseURL: 'http://measure-shelter-api.herokuapp.com/'
+});
 
 export const getData = async (endpoint) => api.get(endpoint);
 

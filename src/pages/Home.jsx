016024c37be0416ae2b45a079cebf6375db6b2c1
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-04 21:48:38 -0300

    refactor: Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import GenericCard from '../templates/GenericCard';
-import SearchForm from '../templates/SearchForm';
-import isleMock from '../mocks/isle';
+import SearchIsle from '../templates/SearchIsle';
 
 export default function Home() {
   return (
     <>
-      <SearchForm />
-      <div className="isles">
-        <div className="isle-cards">
-          { isleMock.map((isle) => (
-            <GenericCard
-              key={isle.name}
-              name={isle.name}
-              imageURL={isle.imageURL}
-              status={isle.status}
-              temperature={isle.temperature}
-              airHumidite={isle.airHumidite}
-              soilHumidite={isle.soilHumidite}
-              dateTime={isle.dateTime}
-            />
-          )) }
-        </div>
-      </div>
+      <SearchIsle />
     </>
   );
 }

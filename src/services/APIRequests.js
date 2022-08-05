bbdf81183f8fadde3ed6ec1ff1fad91996d80ef7
Écio Ferraz <ecioferraz@hotmail.com>
2022-08-05 11:16:34 -0300

    Updates

--- src/services/APIRequests.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://measure-shelter-api.herokuapp.com/'
-});
-
-export const getData = async (endpoint) => api.get(endpoint);
-
-export default api;
--- src/services/formatDate.js
+++
@@ -1,8 +0,0 @@
-import moment from 'moment';
-
-const formatDate = (date) => {
-  const timestamp = new Date(date);
-  return moment(timestamp).format('DD/MM/YYYY');
-};
-
-export default formatDate;
